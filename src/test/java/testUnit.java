f6d737e1b6b988b14c41a407d1c095145b1cec6e
DarkApxumond <eldarprizrak@gmail.com>
2024-11-10 16:23:20 +0300

    test autorization admin

--- src/test/java/testUnit.java
+++ src/test/java/testUnit.java
@@ -1,10 +1,10 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
@@ -17,7 +17,12 @@ public class testUnit {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().setSize(new Dimension(1980, 1020));
-        driver.get("https://www.google.com/");
+        driver.get("http://localhost/litecart/admin/login.php");
+
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
     }
 
     @Test
