1c6784b8ad16e6943947ec9e3173c9f58ac870b2
DarkApxumond <eldarprizrak@gmail.com>
2024-11-10 16:16:35 +0300

    new test

--- src/test/java/testUnit.java
+++ src/test/java/testUnit.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.concurrent.TimeUnit;
 
 public class testUnit {
@@ -15,17 +14,15 @@ public class testUnit {
 
     @Before
     public void start(){
-        ChromeOptions options = new ChromeOptions();
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().window().setSize(new Dimension(1980, 1020));
         driver.get("https://www.google.com/");
     }
 
     @Test
     public void testOne(){
-        
+
     }
 
     @After
